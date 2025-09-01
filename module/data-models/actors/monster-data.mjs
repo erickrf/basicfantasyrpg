1b7e33b04da387ff6ddbe577cae5abc5a53f41e6
Erick Fonseca <erickrfonseca@gmail.com>
2025-09-01 15:43:10 +0200

    Fix monster AB

--- module/data-models/actors/monster-data.mjs
+++ module/data-models/actors/monster-data.mjs
@@ -122,13 +122,15 @@ export class MonsterDataModel extends CreatureDataModel {
     // Calculate effective hit dice for saves
     this.hitDice.effective = this._calculateEffectiveHitDice();
 
+
     // Calculate base XP from hit dice if not manually set
     if (this.xp.value === 0) {
       this.xp.value = this._calculateBaseXP();
     }
 
-    if (this.attackBonus.value === 0) {
-      this.attackBonus.value = this._calculateMonsterAttackBonus();
+    // a given value above 1 should override automatic calculation
+    if (this.attackBonus.value <= 1) {
+      this.attackBonus.value = this.hitDice.effective;
     }
 
     // Calculate monster saves based on hit dice
@@ -182,7 +184,7 @@ export class MonsterDataModel extends CreatureDataModel {
     let xpValue = 0;
     let xpSpecialAbilityBonus = 0;
 
-    if (hitDice.number < 1 || (hitDice.number === 1 && hitDice.mod < 0) || hitDice.size < "d8") {
+    if (this.hitDice.effective === 0) {
       xpValue = xpLookup[0];
       xpSpecialAbilityBonus = specialAbilityLookup[0] * specialAbility;
     } else if (hitDice.number > 25) {
@@ -195,53 +197,6 @@ export class MonsterDataModel extends CreatureDataModel {
 
     xpSpecialAbilityBonus = Math.max(0, xpSpecialAbilityBonus); // never return a negative special ability bonus
     return xpValue + xpSpecialAbilityBonus;
-  }
-
-  /**
-   * Calculate monster attack bonus
-   */
-  _calculateMonsterAttackBonus() {
-    const hitDiceNumber = this.hitDice.number;
-    if (hitDiceNumber < 1) {
-      return 0;
-    } else if (hitDiceNumber > 31) {
-      return 16;
-    }
-    switch (hitDiceNumber) {
-      case 9:
-        return 8;
-      case 10:
-      case 11:
-        return 9;
-      case 12:
-      case 13:
-        return 10;
-      case 14:
-      case 15:
-        return 11;
-      case 16:
-      case 17:
-      case 18:
-      case 19:
-        return 12;
-      case 20:
-      case 21:
-      case 22:
-      case 23:
-        return 13;
-      case 24:
-      case 25:
-      case 26:
-      case 27:
-        return 14;
-      case 28:
-      case 29:
-      case 30:
-      case 31:
-        return 15;
-      default:
-        return hitDiceNumber; // this handles 1-9
-    }
   }
 
   /**
